84d591e0b3cecc5da8e62aa05f3419fb5d73d557
Ayrton Yamashita <ayrtonyamashita@gmail.com>
2025-06-05 15:47:36 -0300

    style: add signup page

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,44 +1,53 @@
+import { useState } from "react";
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
-import { useState } from "react";
 
-export function SignIn() {
+export function SignUp() {
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
   function onSubmit(e: React.FormEvent) {
     e.preventDefault();
-    console.log(email, password);
+    console.log(name, email, password, confirmPassword);
   }
 
   return (
     <form onSubmit={onSubmit} className="w-full flex flex-col gap-4">
       <Input
+        placeholder="Seu nome"
+        legend="Nome"
         required
-        legend="E-mail"
-        type="email"
+        onChange={(e) => setName(e.target.value)}
+      />
+      <Input
         placeholder="seu@email.com"
+        legend="Email"
+        required
         onChange={(e) => setEmail(e.target.value)}
       />
-
       <Input
-        required
-        legend="Senha"
-        type="password"
         placeholder="123456"
+        legend="Senha"
+        required
+        type="password"
         onChange={(e) => setPassword(e.target.value)}
       />
-
-      <Button type="submit" isLoading={isLoading}>
-        Entrar
-      </Button>
-
+      <Input
+        placeholder="123456"
+        legend="Confirme sua senha"
+        required
+        type="password"
+        onChange={(e) => setConfirmPassword(e.target.value)}
+      />
+      <Button type="submit">Cadastrar</Button>
       <a
-        href="/signup"
+        href="/"
         className="text-sm font-semibold text-gray-100 mt-10 mb-4 text-center hover:text-blue-600 transition ease-linear"
       >
-        Criar conta
+        Já tenho uma conta
       </a>
     </form>
   );
