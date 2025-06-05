459730194a0caeeb58fad932a34bc0cfa002c8c0
Ayrton Yamashita <ayrtonyamashita@gmail.com>
2025-06-05 15:25:19 -0300

    style: add link to signup

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -13,6 +13,7 @@ export function Button({
       type={type}
       disabled={isLoading}
       className="flex items-center justify-center bg-blue-500 rounded-lg text-white cursor-pointer hover:bg-blue-400 transition ease-linear disabled:opacity-50 disabled:cursor-progress h-12 disabled:hover:bg-blue-500"
+      {...rest}
     >
       {children}
     </button>
--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,7 +1,17 @@
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
+import { useState } from "react";
 
 export function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  function onSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    console.log(email, password);
+  }
+
   return (
     <form action="" className="w-full flex flex-col gap-4">
       <Input
@@ -9,11 +19,27 @@ export function SignIn() {
         legend="E-mail"
         type="email"
         placeholder="seu@email.com"
+        onChange={(e) => setEmail(e.target.value)}
       />
 
-      <Input required legend="Senha" type="password" placeholder="123456" />
+      <Input
+        required
+        legend="Senha"
+        type="password"
+        placeholder="123456"
+        onChange={(e) => setPassword(e.target.value)}
+      />
 
-      <Button>Entrar</Button>
+      <Button type="submit" isLoading={isLoading}>
+        Entrar
+      </Button>
+
+      <a
+        href="/signup"
+        className="text-sm font-semibold text-gray-100 mt-10 mb-4 text-center hover:text-blue-600 transition ease-linear"
+      >
+        Criar conta
+      </a>
     </form>
   );
 }
